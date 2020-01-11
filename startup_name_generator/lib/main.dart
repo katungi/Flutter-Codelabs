4681e5d53a8c787993862ef57a0858953663b309
katungi <dankatdennis@gmail.com>
2020-01-11 07:25:58 +0300

    added english word generator

--- startup_name_generator/lib/main.dart
+++ startup_name_generator/lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      title: 'Start Up Generator',
+      home: Scaffold(
+        appBar: AppBar(
+          title:  Text('Welcome to Flutter'), 
+          ),
+          body:  Center(
+            child: Text( WordPair.random().asPascalCase),
+          ),
+      ),
     );
   }
 }
